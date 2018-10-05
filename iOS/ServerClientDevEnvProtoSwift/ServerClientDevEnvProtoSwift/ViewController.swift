ead695c7a04f58d4a903fe0a0bb8ba5d95fd2099
Mike Laursen <mailbox@laursen.org>
2018-10-05 14:13:14 -0700

    Call to health check API endpoint is working.

--- iOS/ServerClientDevEnvProtoSwift/ServerClientDevEnvProtoSwift/ViewController.swift
+++ iOS/ServerClientDevEnvProtoSwift/ServerClientDevEnvProtoSwift/ViewController.swift
@@ -17,7 +17,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func doHealthCheck(_ sender: UIButton) {
-        healthCheckLog.text = "Hello, world!"
+//        healthCheckLog.text = "Hello, world!"
+        
+        let url = URL(string: "http://localhost:8080")
+        let dataTask = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            let httpResponse = response as! HTTPURLResponse
+            let statusCode = httpResponse.statusCode
+            NSLog("%ld", statusCode)
+        }
+        dataTask.resume()
     }
 }
 
